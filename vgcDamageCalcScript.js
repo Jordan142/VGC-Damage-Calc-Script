dc81f6fc8730d3c8ba92fda28f4d19d2577f0b00
Jordan Guy <jordan.guy@fdmgroup.com>
2025-06-23 01:50:13 +0100

    Half put in tera and non-tera calcs, need user input still

--- vgcDamageCalcScript.js
+++ vgcDamageCalcScript.js
@@ -1,72 +1,135 @@
 import {calculate, Generations, Pokemon, Move, Field} from '@smogon/calc';
 import fetchTeamData from './convertSDPaste.js';
-import { getKOChance } from '@smogon/calc/dist/desc.js';
+import { display } from '@smogon/calc/dist/desc.js';
+import readline from 'node:readline';
 
-// Moves need to be fixed
+const atkPaste = await fetchTeamData("https://pokepast.es/53a0c693aa5453e3");
 
-const atkPaste = await fetchTeamData("https://pokepast.es/cf23c1115b55728f");
+const defPaste = await fetchTeamData("https://pokepast.es/cf23c1115b55728f");
 
-const defPaste = await fetchTeamData("https://pokepast.es/53a0c693aa5453e3");
+var atkTera;
+var defTera;
+
+// const r1 = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+//   prompt: '> '
+// });
+
+// r1.question("Do you want to show additional damage calcs where your attackers are tera'd? (y/n)", (answer) => {
+//   atkTera = answer.toLowerCase();
+//   r1.close();
+// });
+
+// r1.question("Do you want to show additional damage calcs where the opposition Pokemon are tera'd? (y/n)", (answer) => {
+//   defTera = answer.toLowerCase();
+//   r1.close();
+// });
 
 const gen = Generations.get(9);
 atkPaste.forEach((atkPoke) => {
   const moves = atkPoke.moves
   defPaste.forEach((defPoke) => {
     moves.forEach((move) => {
-      const atkPokemon = new Pokemon(gen, atkPoke.name, {
-          level: 50,
-          item: atkPoke.item,
-          nature: atkPoke.nature,
-          ability: atkPoke.ability,
-          teraType: atkPoke.tera,
-          ivs: atkPoke.IVs,
-          evs: atkPoke.EVs,
-        });
-      const defPokemon = new Pokemon(gen, defPoke.name, {
-          level: 50,
-          item: defPoke.item,
-          nature: defPoke.nature,
-          ability: defPoke.ability,
-          teraType: defPoke.tera,
-          ivs: defPoke.IVs,
-          evs: defPoke.EVs,
-        });
-      const atkMove = new Move(gen, move)
-      const result = calculate(
-        gen,
-        atkPokemon,
-        defPokemon,
-        atkMove,
+      var field = new Field(
+        { gameType: 'Doubles'}
       );
-      if (result["move"]["category"] != "Status") {
-        if (result.damage != 0) {
-          damageDescription(result);
-          const koChance = getKOChance(gen, atkPokemon, defPokemon, atkMove, new Field({defenderSide: {isLightScreen: false}}), result.damage);
-          console.log(koChance);
+      if (atkTera == "y" || atkTera == "yes") {
+        if (defTera == "y" || defTera == "yes") {
+          battleConstructor(gen, atkPoke, defPoke, move, field, "no", "no");
+          battleConstructor(gen, atkPoke, defPoke, move, field, "yes", "no");
+          battleConstructor(gen, atkPoke, defPoke, move, field, "no", "yes");
+          battleConstructor(gen, atkPoke, defPoke, move, field, "yes", "yes");
         } else {
-          console.log("This move does no damage!");
+          battleConstructor(gen, atkPoke, defPoke, move, field, "yes", "no");
+          battleConstructor(gen, atkPoke, defPoke, move, field, "yes", "no");
+        }
+      } else {
+        if (defTera == "y" || defTera == "yes") {
+          battleConstructor(gen, atkPoke, defPoke, move, field, "no", "yes");
+          battleConstructor(gen, atkPoke, defPoke, move, field, "no", "yes");
+        } else {
+          battleConstructor(gen, atkPoke, defPoke, move, field, "no", "no");
         }
       }
-    })
-  })
+      // if (result["move"]["category"] != "Status") {
+      //   if (result.damage != 0) {
+      //     const description = display(gen, atkPokemon, defPokemon, atkMove, field, result.damage, result.rawDesc)
+      //     console.log(description);
+      //   } else {
+      //     console.log("This move does no damage!");
+      //   }
+      // }
+    });
+  });
 });
 
 /*
 TO DO:
-- Find way to calculate OHKO chance
-- Find a way to set it to double battles account for double battles
-  - Iterate over damage calcs for each Pokemon accounting for each partner Pokemon
+- Add user input for tera check
+- Iterate over damage calcs for each Pokemon accounting for each partner Pokemon as a bonus
 - Output final data to a document of some sort
 - Create a simple UI to update the Field attribute
 */
 
-function damageDescription(result) {
-  const desc = result.rawDesc;
-  const damage = result.damage;
-  const defPoke = result.defender;
-  const minDamPerc = Math.round((damage[0] / defPoke.originalCurHP) * 1000)/10;
-  const maxDamPerc = Math.round((damage[15] / defPoke.originalCurHP) * 1000)/10;
-  const baseOutput = desc.attackEVs + " " + desc.attackerName + " " + desc.moveName + " vs. " + desc.HPEVs + "/" + desc.defenseEVs + " " + desc.defenderName + ": " + damage[0] + "-" + damage[15] + " (" + minDamPerc + " - " + maxDamPerc + "%)"; 
-  console.log(baseOutput);
-  console.log(damage);
+function battleConstructor(gen, atkPoke, defPoke, move, field, atkTeraCheck, defTeraCheck) {
+  var atkPokemon;
+  var defPokemon;
+  if (atkTeraCheck == "y" || atkTeraCheck == "yes") {
+    atkPokemon = new Pokemon(gen, atkPoke.name, {
+      level: 50,
+      item: atkPoke.item,
+      nature: atkPoke.nature,
+      ability: atkPoke.ability,
+      teraType: atkPoke.tera,
+      ivs: atkPoke.IVs,
+      evs: atkPoke.EVs,
+    });
+  } else {
+    atkPokemon = new Pokemon(gen, atkPoke.name, {
+      level: 50,
+      item: atkPoke.item,
+      nature: atkPoke.nature,
+      ability: atkPoke.ability,
+      ivs: atkPoke.IVs,
+      evs: atkPoke.EVs,
+    });
+  }
+  if (defTeraCheck == "y" || defTeraCheck == "yes") {
+    defPokemon = new Pokemon(gen, defPoke.name, {
+      level: 50,
+      item: defPoke.item,
+      nature: defPoke.nature,
+      ability: defPoke.ability,
+      teraType: defPoke.tera,
+      ivs: defPoke.IVs,
+      evs: defPoke.EVs,
+    });
+  } else {
+    defPokemon = new Pokemon(gen, defPoke.name, {
+      level: 50,
+      item: defPoke.item,
+      nature: defPoke.nature,
+      ability: defPoke.ability,
+      ivs: defPoke.IVs,
+      evs: defPoke.EVs,
+    });
+  }
+  const atkMove = new Move(gen, move);
+  const result = calculate(
+    gen,
+    atkPokemon,
+    defPokemon,
+    atkMove,
+    field
+  );
+
+  if (result["move"]["category"] != "Status") {
+    if (result.damage != 0) {
+      const description = display(gen, atkPokemon, defPokemon, atkMove, field, result.damage, result.rawDesc);
+      console.log(description);
+    } else {
+      console.log("This move does no damage!");
+    }
+  }
 }
