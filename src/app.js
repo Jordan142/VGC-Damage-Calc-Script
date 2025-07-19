e3e5b9d15ff5285132e6c47658abe67024e511ca
Jordan Guy <jordan.guy@fdmgroup.com>
2025-07-19 18:41:37 +0100

    Set up project to be able to create binary files and updated repo layout

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import genCalcs from './vgcDamageCalcScript.js';
+
+/* Example pastes:
+https://pokepast.es/53a0c693aa5453e3 - Zacian Paste
+https://pokepast.es/cf23c1115b55728f - Zam Paste
+https://pokepast.es/a6f455eb0c255c08 - Terapagos/Stellar Tera Paste
+*/
+
+genCalcs();
